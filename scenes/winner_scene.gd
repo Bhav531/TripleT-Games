ae78b039bdb84a4e5acc588fef5050c8bb1d3ad7
Bhav_Jindal <bhav.kartik@gmail.com>
2026-07-22 09:48:24 +0400

    final touch

--- scenes/winner_scene.gd
+++ scenes/winner_scene.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 func _on_retry_pressed() -> void:
-	# 1. CRITICAL: Unpause the game so inputs and buttons start working again!
+	# 1. Unpause the game just in case it was paused
 	get_tree().paused = false
 	
-	# 2. Reset global stats back to fresh starting values
+	# 2. Fully reset your global stats
 	Global.lives = 5
 	Global.minigames_done = 0
 	
-	# 3. Go back to the title screen cleanly
-	get_tree().change_scene_to_file("res://scenes/title_scene.tscn")
+	# 3. Jump straight back into the first minigame (bypassing the title screen completely!)
+	get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
+	# (Note: If your game starts at level_scene.tscn instead, change minigame_1.tscn to level_scene.tscn)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
